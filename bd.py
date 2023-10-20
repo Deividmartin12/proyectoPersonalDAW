800d84185cf003ad600cb0584967b37a82d005cd
Deividmartin12 <131992849+Deividmartin12@users.noreply.github.com>
2023-10-20 08:06:43 -0500

    contraseña

--- bd.py
+++ bd.py
@@ -3,5 +3,5 @@ import pymysql
 def obtener_conexion():
     return pymysql.connect(host='DavidCornejo.mysql.pythonanywhere-services.com',
                                 user='DavidCornejo',
-                                password='123456789',
+                                password='davidmartin12',
                                 db='DavidCornejo$discos')
