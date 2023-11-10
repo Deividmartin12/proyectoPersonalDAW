77c1a211aa7d5713b7ecbd00a22542012a3110bb
Deividmartin12 <131992849+Deividmartin12@users.noreply.github.com>
2023-11-10 08:03:21 -0500

    A

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 from flask import Flask, render_template, request, redirect, flash, jsonify
+from flask_jwt import JWT, jwt_required, current_identity
 from markupsafe import escape
 import math
 import controladores.controlador_discos as controlador_discos
@@ -9,8 +10,43 @@ from flask import url_for
 from flask import make_response
 import hashlib
 import random
+######################## SEGURIDAD - INICIO #######################################
+
+class User(object):
+    def __init__(self, id, username, password):
+        self.id = id
+        self.username = username
+        self.password = password
+
+    def __str__(self):
+        return "User(id='%s')" % self.id
+
+users = [
+    User(1, 'user1', 'abcxyz'),
+    User(2, 'user2', 'abcxyz'),
+]
+
+username_table = {u.username: u for u in users}
+userid_table = {u.id: u for u in users}
+
+def authenticate(username, password):
+    user = username_table.get(username, None)
+    if user and safe_str_cmp(user.password.encode('utf-8'), password.encode('utf-8')):
+        return user
+
+def identity(payload):
+    user_id = payload['identity']
+    return userid_table.get(user_id, None)
 
 app = Flask(__name__)
+app.debug = True
+app.config['SECRET_KEY'] = 'super-secret'
+
+jwt = JWT(app, authenticate, identity)
+######################## SEGURIDAD - FIN #######################################
+ 
+
+
 
 @app.route("/pruebajson")
 def pruebajson():
